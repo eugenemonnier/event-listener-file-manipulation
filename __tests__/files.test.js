54a46e9e5c8f86c5cd9808afc8918d6d289aa989
Eugene Monnier <eugene.monnier@gmail.com>
2020-03-03 10:49:15 -0800

    added additional tests

--- __tests__/files.test.js
+++ __tests__/files.test.js
@@ -10,18 +10,51 @@ describe('files module', () => {
   })
 
   it('can save a file', () => {
-
+    const buffer = Buffer.from('test')
+    return files.saveFile('real_legit_file.txt', buffer)
+      .then(success => {
+        expect(success).toBeUndefined()
+      })
+      .catch(err => {
+        expect(err).toBeUndefined()
+      })
   })
 
   it('raises an error if a file is invalid', () => {
-
+    const buffer = Buffer.from('test')
+    return files.saveFile(null, buffer)
+      .then(success => {
+        expect(success).toBeUndefined()
+      })
+      .catch(err => {
+        expect(err.message).toEqual('Invalid file')
+      })
   })
 
   it('can uppercase a buffer of text', () => {
-
+    const lowercase = Buffer.from('abcdef')
+    const uppercase = Buffer.from('ABCDEF')
+    expect(files.convertBuffer(lowercase)).toEqual(Buffer.from(uppercase))
   })
+  describe('alterFile', () => {
+    it('works when a good filename is passed', () => {
+      return files.alterFile('real_legit_file.txt')
+        .then(success => {
+          expect(success).toBeUndefined()
+        })
+        .catch(err => {
+          expect(err).toBeUndefined()
+        })
+    })
 
-  it('can alter a file', () => {
-
+    it('fails when a bad filename is passed', () => {
+      return files.alterFile(null)
+        .then(success => {
+          expect(success).toBeFalsy()
+        })
+        .catch(err => {
+          expect(err.message).toEqual('Invalid file')
+        })
+    })
   })
 })
